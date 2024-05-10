a91d7e3c1c6cfb7f262f634b53d118d15aea929b
Kenny <40118685+KennnyTroll@users.noreply.github.com>
2024-05-11 00:54:57 +0200

    Update settings.hpp

--- src/core/settings.hpp
+++ src/core/settings.hpp
@@ -290,6 +290,8 @@ namespace big
 			bool force_relay_connections = false;
 			bool stop_traffic            = true;
 			bool desync_kick             = false;
+			std::string freeze_model     = "prop_thindesertfiller_aa";
+			std::string freeze_fake_model = "ch_prop_ch_diamond_xmastree";	
 
 			NLOHMANN_DEFINE_TYPE_INTRUSIVE(protections, script_events, rid_join, receive_pickup, admin_check, kick_rejoin, force_relay_connections, stop_traffic, desync_kick)
 		} protections{};
